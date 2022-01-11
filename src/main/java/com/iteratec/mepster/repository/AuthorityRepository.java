2ec2303756bdd36d92e679ead951bec66a709444
Karsten Schwank <karsten.schwank@iteratec.com>
2022-01-11 13:23:24 +0100

    initial commit
    freshly generated project

---
+++ src/main/java/com/iteratec/mepster/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.iteratec.mepster.repository;
+
+import com.iteratec.mepster.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/test/java/com/iteratec/mepster/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.iteratec.mepster;
+
+import com.iteratec.mepster.MepsterApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = MepsterApp.class)
+public @interface IntegrationTest {
+}
